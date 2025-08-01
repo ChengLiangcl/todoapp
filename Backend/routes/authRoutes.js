5e7ee434d17b5401470aef4bf3c1e44881cf4e98
Cheng Liang <513561970@qq.com>
2025-08-01 20:14:50 +1000

    merge frontend and backend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+//Defined the login route
 router.post('/login', authController.login);
 router.post('/register', authController.createUser);
 
