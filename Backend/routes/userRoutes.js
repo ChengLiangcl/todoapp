2e0f47de39bc1d19185a7d5506b3852b682f84f5
chenglianglc <513561970@qq.com>
2025-08-06 16:15:29 +1000

    update the cron job

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const isAuth = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
 router.get('/users', isAuth, userController.getAllUsers);
-// router.post('/create-user', userController.createUser);
+// router.post('/users', userController.);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
