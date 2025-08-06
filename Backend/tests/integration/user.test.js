aeca15583216144191f21cfe02c0d0c72f2b1d79
chenglianglc <513561970@qq.com>
2025-08-06 16:15:29 +1000

    update the cron job

--- Backend/tests/integration/user.test.js
+++ Backend/tests/integration/user.test.js
@@ -12,29 +12,29 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe('User API Create ', () => {
-  const userCreateAPI = '/users/';
-  it('POST /users/ - Create a existing user with existing username', async () => {
-    const res = await request(app).post(userCreateAPI).send({
-      username: 'jaypark',
-      email: 'jay.park@testing.com',
-      name: 'Jason Liang',
-    });
-    expect(res.body.code).toBe(400);
-    expect(res.body.message).toBe(
-      "username can't be duplicated, try to enter a unique username"
-    );
-  });
+describe('User API - Create User', () => {
+  const userCreateAPI = '/auth/register'; // or '/auth/register' if that's correct
 
-  it('POST /users/ - Create a existing user with existing email', async () => {
+  it('should return 400 when creating a user with an existing username', async () => {
     const res = await request(app).post(userCreateAPI).send({
-      username: 'jayparkdope',
-      email: 'jay.park@testing.com',
-      name: 'Jason Liang',
+      username: 'cheng.liang',
+      email: 'cheng.liang@testing.com',
+      password: 'Big@ss911111',
+      confirmPassword: 'Big@ss911111',
     });
-    expect(res.body.code).toBe(400);
-    expect(res.body.message).toBe(
-      "email can't be duplicated, try to enter a unique email"
-    );
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe('username cheng.liang already exists');
   });
 });
+
+// it('POST /users/ - Create a existing user with existing email', async () => {
+//   const res = await request(app).post(userCreateAPI).send({
+//     username: 'jayparkdope',
+//     email: 'jay.park@testing.com',
+//     name: 'Jason Liang',
+//   });
+//   expect(res.body.code).toBe(400);
+//   expect(res.body.message).toBe(
+//     "email can't be duplicated, try to enter a unique email"
+//   );
+// });
